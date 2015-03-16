e07d952e3492c7f811af09c9a5352571964331de
Ming Wong <ming.wong@laxino.com>
2015-03-16 18:10:44 +0800

    init rails envirnment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmsRws::Application.config.secret_token = '9ec55c80fc300973563061b5e8209c3bab6458f5fccce494cbb7a884223d277286faed1dddbbe55d58649070cc9aee590e9091a16366906cc448815de0dcd8e0'

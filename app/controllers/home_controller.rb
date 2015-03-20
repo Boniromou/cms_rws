01d8797237cc84a36d4d6850fbf8d86dcbc4616c
Ming Wong <ming.wong@laxino.com>
2015-03-20 16:22:17 +0800

    add cage layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    respond_to do |format|
+      format.html { render file: "home/index", :layout => "cage", formats: [:html] }
+    end
   end
 end

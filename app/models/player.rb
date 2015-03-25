6d4dbe24128a80146dabedf84c304200804f6ebf
Ming Wong <ming.wong@laxino.com>
2015-03-25 17:39:32 +0800

    show create error message by locale key

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,14 +1,19 @@
 class Player < ActiveRecord::Base
   attr_accessible :balance, :card_id, :currency_id,:member_id, :player_name, :status
   def self.create_by_param(member_id,player_name)
-    raise ArgumentError if member_id.blank?
-    raise ArgumentError if player_name.blank?
+    raise ArgumentError , "create_player_error.id_blank_error" if member_id.blank?
+    raise ArgumentError , "create_player_error.name_blank_error" if player_name.blank?
     player = new
     player.member_id = member_id
     player.player_name = player_name
     player.balance = 0
     player.currency_id = 1
     player.status = "unlock"
-    player.save
+    result = false
+    begin
+      player.save
+    rescue
+      raise Exception, "create_player.exist"
+    end
   end
 end

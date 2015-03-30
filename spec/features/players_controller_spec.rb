4dee1d8c2ca27e12145de91d998211e378ac522e
Ming Wong <ming.wong@laxino.com>
2015-03-30 18:26:22 +0800

    add testcase 3.1 show create player page

--- spec/features/players_controller_spec.rb
+++ spec/features/players_controller_spec.rb
@@ -22,7 +22,14 @@ describe PlayersController do
       Player.delete_all
     end
 
-    it '[3.1] show' do
+    it '[3.1] Show Create Player Page' do
+      login_as(@root_user)
+      visit home_path
+      click_link I18n.t("tree_panel.create_player")
+      title = first("div div h1")
+      expect(title.text).to eq I18n.t("tree_panel.create_player")
+      expect(page.source).to have_selector("form#new_player div input#player_member_id")
+      expect(page.source).to have_selector("form#new_player div input#player_name")
     end
   end
 end

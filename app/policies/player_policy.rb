d5516e3e03821b7a20bef781cd0debfd94a593f2
Ming Wong <ming.wong@laxino.com>
2015-03-30 14:37:09 +0800

    add balance permission

--- app/policies/player_policy.rb
+++ app/policies/player_policy.rb
@@ -2,4 +2,9 @@ class PlayerPolicy < ApplicationPolicy
   def create?
     is_admin? || has_permission?('Player', 'create')
   end
+
+  def balance?
+    p "check balance"
+    is_admin? || has_permission?('Player', 'balance')
+  end
 end

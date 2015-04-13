a6d2b04f9fb801174d1056a53f3cff9d7228bc05
Thomas Wong <thomas.wong@laxino.com>
2015-04-13 18:05:23 +0800

    Fix indent

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,7 +1,7 @@
 module ControllerHelpers
     def login_root
       before(:each) do
-	@request.env["devise.mapping"] = Devise.mappings[:system_user]
+        @request.env["devise.mapping"] = Devise.mappings[:system_user]
         root = SystemUser.find_by_username("mo\portal.admin")
         sign_in root # Using factory girl as an example
         @current_user = root

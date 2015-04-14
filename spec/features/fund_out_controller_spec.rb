15e4dcc65e625448d2e2f5ea449ebc2b9ff1fb93
Ming Wong <ming.wong@laxino.com>
2015-04-14 17:51:56 +0800

    add fund out controller test cases

--- spec/features/fund_out_controller_spec.rb
+++ spec/features/fund_out_controller_spec.rb
@@ -1,6 +1,6 @@
 require "feature_spec_helper"
 
-describe FundInController do
+describe FundOutController do
   before(:all) do
     include Warden::Test::Helpers
     Warden.test_mode!
@@ -15,13 +15,13 @@ describe FundInController do
     Warden.test_reset!
   end
 
-  describe '[6] Deposit' do
+  describe '[7] Withdraw' do
     before(:each) do
       AuditLog.delete_all
       PlayerTransaction.delete_all
       Player.delete_all
-      @player = Player.create!(:player_name => "test", :member_id => "123456", :currency_id => 1,:balance => 0, :status => "unlock")
-      TransactionType.create!(:name => "Deposit")
+      @player = Player.create!(:player_name => "test", :member_id => "123456", :currency_id => 1,:balance => 20000, :status => "unlock")
+      TransactionType.create!(:name => "Withdrawal")
     end
     
     after(:each) do
@@ -30,7 +30,7 @@ describe FundInController do
       Player.delete_all
     end
 
-    it '[6.1] show Deposit page' do
+    it '[7.1] show Withdraw page' do
       login_as(@root_user)
       visit home_path
       click_link I18n.t("tree_panel.balance")
@@ -39,53 +39,66 @@ describe FundInController do
       check_title("tree_panel.balance")
 
       within "div#content" do
-        click_link I18n.t("button.deposit")
+        click_link I18n.t("button.withdrawal")
       end
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(find("label#player_name").text).to eq @player.player_name
       expect(find("label#player_member_id").text).to eq @player.member_id.to_s
       expect(page.source).to have_selector("button#confirm")
       expect(page.source).to have_selector("button#cancel")
     end
     
-    it '[6.2] Invalid Deposit', :js => true do
+    it '[7.2] Invalid Withdraw', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 0.111
       expect(find("input#player_transaction_amount").value).to eq "0.11"
     end
 
-    it '[6.3] Invalid Deposit(eng)', :js => true do
+    it '[7.3] Invalid Withdraw(eng)', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => "abc3de"
       expect(find("input#player_transaction_amount").value).to eq ""
     end
 
-    it '[6.4] Invalid Deposit (input 0 amount)', :js => true do
+    it '[7.4] Invalid Withdraw (input 0 amount)', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 0
       click_button I18n.t("button.confirm")
       find("div#confirm_fund_dialog")[:style].include?("block").should == true
       find("div#confirm_fund_dialog div button#confirm").click
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(find("label#player_name").text).to eq @player.player_name
       expect(find("label#player_member_id").text).to eq @player.member_id.to_s
-      check_flash_message I18n.t("invalid_amt.deposit")
+      check_flash_message I18n.t("invalid_amt.withdrawal")
     end
 
-    it '[6.5] cancel Deposit' do
+    it '[7.5] Invalid Withdraw (invalid balance)', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
+      fill_in "player_transaction_amount", :with => 300
+      click_button I18n.t("button.confirm")
+      find("div#confirm_fund_dialog")[:style].include?("block").should == true
+      find("div#confirm_fund_dialog div button#confirm").click
+      check_title("tree_panel.fund_out")
+      expect(find("label#player_name").text).to eq @player.player_name
+      expect(find("label#player_member_id").text).to eq @player.member_id.to_s
+      check_flash_message I18n.t("invalid_amt.withdrawal")
+    end
+
+    it '[7.6] cancel Withdraw' do
+      login_as(@root_user) 
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       find("div#button_set form input#cancel").click
 
       check_title("tree_panel.home")
     end
 
-    it '[6.6] Confirm Deposit', :js => true do
+    it '[7.7] Confirm Withdraw', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       find("div#confirm_fund_dialog")[:style].include?("block").should == true
@@ -96,9 +109,9 @@ describe FundInController do
       expect(page).to have_selector("div#confirm_fund_dialog div button#cancel")
     end
 
-    it '[6.7] cancel dialog box Deposit', :js => true do
+    it '[7.8] cancel dialog box Withdraw', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       find("div#confirm_fund_dialog")[:style].include?("block").should == true
@@ -114,9 +127,9 @@ describe FundInController do
     end
 
 
-    it '[6.8] Confirm Deposit', :js => true do
+    it '[7.9] Confirm Withdraw', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       expect(find("div#confirm_fund_dialog")[:style].include?("block")).to eq true
@@ -126,16 +139,15 @@ describe FundInController do
       expect(page).to have_selector("div#confirm_fund_dialog div button#confirm")
       expect(page).to have_selector("div#confirm_fund_dialog div button#cancel")
       find("div#confirm_fund_dialog div button#confirm").click
-      
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(page).to have_selector("table")
       expect(page).to have_selector("button#print_slip")
       expect(page).to have_selector("a#close_link")
     end
 
-    it '[6.9] audit log for confirm dialog box Deposit', :js => true do
+    it '[7.10] audit log for confirm dialog box Withdraw', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=" + @player.member_id
+      visit fund_out_path + "?member_id=" + @player.member_id
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       find("div#confirm_fund_dialog div button#confirm").click
@@ -148,7 +160,7 @@ describe FundInController do
       audit_log.audit_target.should == "player"
       audit_log.action_by.should == @root_user.employee_id
       audit_log.action_type.should == "update"
-      audit_log.action.should == "deposit"
+      audit_log.action.should == "withdrawal"
       audit_log.action_status.should == "success"
       audit_log.action_error.should be_nil
       audit_log.ip.should_not be_nil
@@ -156,11 +168,11 @@ describe FundInController do
       audit_log.description.should_not be_nil
     end
 
-    it '[6.10] click unauthorized action (Deposit)' do
+    it '[7.11] click unauthorized action (Withdraw)' do
       @test_user = User.create!(:uid => 2, :employee_id => 'test.user')
       login_as_not_admin(@test_user)
       set_permission(@test_user,"cashier",:player,["balance"])
-      set_permission(@test_user,"cashier",:player_transaction,["deposit"])
+      set_permission(@test_user,"cashier",:player_transaction,["withdraw"])
       visit home_path
       click_link I18n.t("tree_panel.balance")
       fill_in "player_member_id", :with => @player.member_id
@@ -172,27 +184,27 @@ describe FundInController do
       set_permission(@test_user,"cashier",:player,[])
       set_permission(@test_user,"cashier",:player_transaction,[])
 
-      find("div a#balance_deposit").click
+      find("div a#balance_withdraw").click
 
       check_title("tree_panel.home")
       check_flash_message I18n.t("flash_message.not_authorize")
     end
 
-    it '[6.11] click link to the unauthorized page' do
+    it '[7.12] click link to the unauthorized page' do
       @test_user = User.create!(:uid => 2, :employee_id => 'test.user')
       login_as_not_admin(@test_user)
       set_permission(@test_user,"cashier",:player_transaction,[])
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       check_title("tree_panel.home")
       check_flash_message I18n.t("flash_message.not_authorize")
     end
 
-    it '[6.12] click unauthorized action (confirm dialog box Deposit)', :js => true do
+    it '[7.13] click unauthorized action (confirm dialog box Withdraw)', :js => true do
       @test_user = User.create!(:uid => 2, :employee_id => 'test.user')
       login_as_not_admin(@test_user)
       set_permission(@test_user,"cashier",:player,["balance"])
-      set_permission(@test_user,"cashier",:player_transaction,["deposit"])
-      visit fund_in_path + "?member_id=" + @player.member_id
+      set_permission(@test_user,"cashier",:player_transaction,["withdraw"])
+      visit fund_out_path + "?member_id=" + @player.member_id
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       set_permission(@test_user,"cashier",:player_transaction,[])
@@ -203,11 +215,11 @@ describe FundInController do
       check_flash_message I18n.t("flash_message.not_authorize")
     end
     
-    it '[6.13] click unauthorized action (print slip)', :js => true do
+    it '[7.14] click unauthorized action (print slip)', :js => true do
       @test_user = User.create!(:uid => 2, :employee_id => 'test.user')
       login_as_not_admin(@test_user)
-      set_permission(@test_user,"cashier",:player_transaction,["deposit"])
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      set_permission(@test_user,"cashier",:player_transaction,["withdraw"])
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       expect(find("div#confirm_fund_dialog")[:style].include?("block")).to eq true
@@ -218,15 +230,15 @@ describe FundInController do
       expect(page).to have_selector("div#confirm_fund_dialog div button#cancel")
       find("div#confirm_fund_dialog div button#confirm").click
       
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(page).to have_selector("table")
       expect(page).to_not have_selector("button#print_slip")
       expect(page).to have_selector("a#close_link")
     end
 
-    it '[6.14] Print slip', :js => true do
+    it '[7.15] Print slip', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       expect(find("div#confirm_fund_dialog")[:style].include?("block")).to eq true
@@ -237,7 +249,7 @@ describe FundInController do
       expect(page).to have_selector("div#confirm_fund_dialog div button#cancel")
       find("div#confirm_fund_dialog div button#confirm").click
       
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(page).to have_selector("table")
       expect(page).to have_selector("button#print_slip")
       expect(page).to have_selector("a#close_link")
@@ -252,9 +264,9 @@ describe FundInController do
       check_title("tree_panel.home")
     end
 
-    it '[6.15] Close slip', :js => true do
+    it '[7.16] Close slip', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       expect(find("div#confirm_fund_dialog")[:style].include?("block")).to eq true
@@ -265,7 +277,7 @@ describe FundInController do
       expect(page).to have_selector("div#confirm_fund_dialog div button#cancel")
       find("div#confirm_fund_dialog div button#confirm").click
       
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(page).to have_selector("table")
       expect(page).to have_selector("button#print_slip")
       expect(page).to have_selector("a#close_link")
@@ -275,9 +287,9 @@ describe FundInController do
       check_title("tree_panel.home")
     end
     
-    it '[6.16] audit log for print slip', :js => true do
+    it '[7.17] audit log for print slip', :js => true do
       login_as(@root_user) 
-      visit fund_in_path + "?member_id=#{@player.member_id}"
+      visit fund_out_path + "?member_id=#{@player.member_id}"
       fill_in "player_transaction_amount", :with => 100
       click_button I18n.t("button.confirm")
       expect(find("div#confirm_fund_dialog")[:style].include?("block")).to eq true
@@ -288,7 +300,7 @@ describe FundInController do
       expect(page).to have_selector("div#confirm_fund_dialog div button#cancel")
       find("div#confirm_fund_dialog div button#confirm").click
       
-      check_title("tree_panel.fund_in")
+      check_title("tree_panel.fund_out")
       expect(page).to have_selector("table")
       expect(page).to have_selector("button#print_slip")
       expect(page).to have_selector("a#close_link")

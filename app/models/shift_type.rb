7a28be7d5a2927bf50f180a84d90793d204c5b8c
Thomas Wong <thomas.wong@laxino.com>
2015-04-27 14:40:34 +0800

    Add models Shift, ShiftType, AccountingDate

--- app/models/shift_type.rb
+++ app/models/shift_type.rb
@@ -1,3 +1,13 @@
 class ShiftType < ActiveRecord::Base
   attr_accessible :name
+
+  class << self
+    def get_id_by_name( shift_name )
+      ShiftType.find_by_name(shift_name).id
+    end
+
+    def get_name_by_id( id )
+      ShiftType.find_by_id(id).name
+    end
+  end
 end

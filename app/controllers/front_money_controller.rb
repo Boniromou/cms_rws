7c6cc3c36cdd747b65b2b52cffce8a51a7c92323
Ming Wong <ming.wong@laxino.com>
2015-04-29 10:45:46 +0800

    add front money search result

--- app/controllers/front_money_controller.rb
+++ app/controllers/front_money_controller.rb
@@ -1,5 +1,15 @@
 class FrontMoneyController < ApplicationController
   layout 'cage'
   def search
+    
+  end
+
+  def do_search
+    account_date = params[:accounting_date]
+    shift_name = params[:shift_name]
+    respond_to do |format|
+      format.html { render partial: "front_money/search_result", formats: [:html] }
+      format.js { render partial: "front_money/search_result", formats: [:js] }
+    end
   end
 end
--- app/controllers/player_transactions_controller.rb
+++ app/controllers/player_transactions_controller.rb
@@ -8,25 +8,15 @@ class PlayerTransactionsController < ApplicationController
 
   def do_search
     return unless permission_granted? PlayerTransaction.new, :search?
-    begin
-      id_type = params[:id_type]
-      id_number = params[:id_number]
-      start_time = parse_datetime(params[:start_time]).utc unless params[:start_time].blank?
-      end_time = parse_datetime(params[:end_time]).utc unless params[:end_time].blank?
-      transaction_id = params[:transaction_id]
-      @player_transactions = PlayerTransaction.search_query(id_type, id_number, start_time, end_time, transaction_id)
-      respond_to do |format|
-        format.html { render partial: "player_transactions/search_result", formats: [:html] }
-        format.js { render partial: "player_transactions/search_result", formats: [:js] }
-      end
-    rescue Exception => e
-      puts e.message
-      puts e.backtrace
-      respond_to do |format|
-        format.html { render partial: "player_transactions/search_result", formats: [:html] }
-        format.js { render partial: "player_transactions/search_result", formats: [:js] }
-      end
-
+    id_type = params[:id_type]
+    id_number = params[:id_number]
+    start_time = parse_datetime(params[:start_time]).utc unless params[:start_time].blank?
+    end_time = parse_datetime(params[:end_time]).utc unless params[:end_time].blank?
+    transaction_id = params[:transaction_id]
+    @player_transactions = PlayerTransaction.search_query(id_type, id_number, start_time, end_time, transaction_id)
+    respond_to do |format|
+      format.html { render partial: "player_transactions/search_result", formats: [:html] }
+      format.js { render partial: "player_transactions/search_result", formats: [:js] }
     end
   end
 

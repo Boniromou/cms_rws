9096f5b23dd52ed65a582e63d789e1ef8a118155
Ming Wong <ming.wong@laxino.com>
2015-05-04 16:07:33 +0800

    add search by card id test cases

--- spec/features/players_controller_spec.rb
+++ spec/features/players_controller_spec.rb
@@ -386,4 +386,61 @@ describe PlayersController do
       first("aside#left-panel ul li#nav_balance_enquiry").should be_nil
     end     
   end
+  
+  describe '[12] Search player by card ID' do
+    before(:each) do
+      clean_dbs
+      create_shift_data
+      mock_cage_info
+    end
+
+    after(:each) do
+      Player.delete_all
+    end
+
+    it '[12.1] Show search Page' do
+      login_as_admin
+      visit home_path
+      click_link I18n.t("tree_panel.balance")
+      check_search_page
+    end
+
+    it '[12.2] successfully search player' do
+      @player = Player.create!(:player_name => "exist", :member_id => 123456, :card_id => 1234567890, :currency_id => 1, :balance => 0, :status => "active")
+      login_as_admin
+      visit players_search_path + "?operation=balance"
+      fill_search_info("card_id", @player.card_id)
+      click_button I18n.t("button.find")
+      check_balance_page
+      check_player_info
+    end
+    
+    it '[12.3] fail to search player' do
+      @player = Player.new
+      @player.member_id = 123456
+      @player.card_id = 1234567890
+      @player.player_name = "test player"
+      login_as_admin
+      visit players_search_path + "?operation=balance"
+      fill_search_info("card_id", @player.card_id)
+      click_button I18n.t("button.find")
+      check_not_found
+      click_link I18n.t("button.create")
+    end
+    
+    it '[12.4] direct to create player' do
+      @player = Player.new
+      @player.member_id = 123456
+      @player.card_id = 1234567890
+      @player.player_name = "test player"
+      login_as_admin
+      visit players_search_path + "?operation=balance"
+      fill_search_info("card_id", @player.card_id)
+      click_button I18n.t("button.find")
+      check_not_found
+      click_link I18n.t("button.create")
+      check_title("tree_panel.create_player")
+      expect(find("form#new_player input#player_card_id").value).to eq @player.card_id.to_s
+    end
+  end
 end

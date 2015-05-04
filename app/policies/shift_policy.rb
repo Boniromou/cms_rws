8f70740bda1b20bb7adf835e5c20c4f92197f122
Ming Wong <ming.wong@laxino.com>
2015-05-04 11:13:12 +0800

    add search fm report permission limit

--- app/policies/shift_policy.rb
+++ app/policies/shift_policy.rb
@@ -2,4 +2,12 @@ class ShiftPolicy < ApplicationPolicy
   def roll?
     is_admin? || has_permission?('shift', 'roll')
   end
+
+  def search_fm?
+    is_admin? || has_permission?('shift', 'search_fm')
+  end
+
+  def print_fm?
+    is_admin? || has_permission?('shift', 'print_fm')
+  end
 end

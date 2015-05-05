98914328673cf5fc8293ad9d2f4339254fae8851
Thomas Wong <thomas.wong@laxino.com>
2015-05-05 11:54:34 +0800

    Add error 404 and 500 pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,22 @@ class ApplicationController < ActionController::Base
   include Pundit
   include CageInfoHelper
 
+  rescue_from Exception, :with => :handle_fatal_error
+
   def client_ip
     if Rails.env.development?
       request.remote_ip
     else
       request.env["HTTP_X_FORWARDED_FOR"]
-     end
-   end
+    end
+  end
+
+  def handle_route_not_found
+    respond_to do |format|
+      format.html { render partial: "shared/error404", formats: [:html], layout: "error_page", :status => :not_found }
+      format.js { render partial: "shared/error404", formats: [:js], :status => :not_found }
+    end
+  end
   
   protected
 
@@ -47,5 +56,16 @@ class ApplicationController < ActionController::Base
       return false
     end
     true
+  end
+
+  def handle_fatal_error(e)
+    @from = params[:from]
+    Rails.logger.error "#{e.message}"
+    Rails.logger.error "#{e.backtrace.inspect}"
+    respond_to do |format|
+      format.html { render partial: "shared/error500", formats: [:html], layout: "error_page", :status => :internal_server_error }
+      format.js { render partial: "shared/error500", formats: [:js], :status => :internal_server_error }
+    end
+    return
   end
 end

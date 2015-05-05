e7ec98a588686fb66538e071589b3bc91957ddff
Ming Wong <ming.wong@laxino.com>
2015-05-05 18:36:18 +0800

    change icon on search page

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,11 +1,15 @@
 module ViewsHelper
   def close_balance
-    icon = content_tag(:i,"", :class => "fa fa-times")
+    icon = create_icon("fa fa-times")
     content_tag(:a, icon, :href => home_path, "data-remote".to_sym => true, :id => "balance", :class => "btn btn-primary")
   end
 
   def close_fund_request
-    icon = content_tag(:i,"", :class => "fa fa-times")
+    icon = create_icon("fa fa-times")
     content_tag(:a, icon, :href =>  balance_path + "?member_id=#{@player.member_id}", "data-remote".to_sym => true, :id => "cancel", :class => "btn btn-primary")
+  end
+
+  def create_icon(style)
+    content_tag(:i,"", :class => style)
   end
 end

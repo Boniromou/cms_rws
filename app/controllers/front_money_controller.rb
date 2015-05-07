1f89f382a92a662f13c5b5ebf70e4a5cba0da1bd
Ming Wong <ming.wong@laxino.com>
2015-05-07 10:19:58 +0800

    add default accounting date on FM report

--- app/controllers/front_money_controller.rb
+++ app/controllers/front_money_controller.rb
@@ -10,9 +10,10 @@ class FrontMoneyController < ApplicationController
   def do_search
     begin
     accounting_date = params[:accounting_date]
+    @accounting_date = parse_date(accounting_date, current_accounting_date.accounting_date)
     shift_name = params[:shift_name]
     shift_type_id = ShiftType.get_id_by_name(shift_name)
-    accounting_date_id = AccountingDate.get_id_by_date(accounting_date)
+    accounting_date_id = AccountingDate.get_id_by_date(@accounting_date)
     shift = Shift.find_by_accounting_date_id_and_shift_type_id(accounting_date_id, shift_type_id)
 
     @player_transaction_group = PlayerTransaction.search_transactions_group_by_station(shift.id)

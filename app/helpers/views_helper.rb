829d51e3a4631c8cb1c525235868da1a68084af7
Ming Wong <ming.wong@laxino.com>
2015-05-08 11:35:46 +0800

    add bread crub

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -12,4 +12,20 @@ module ViewsHelper
   def create_icon(style)
     content_tag(:i,"", :class => style)
   end
+
+  def bread_crub(icon_style,title,subtitle)
+    text = ""
+    if subtitle.class == String
+      subtitle = [subtitle]
+    end
+
+    subtitle.each do |str|
+      text += " > " + str
+    end
+    subtitle_content = content_tag(:span, text)
+    icon = create_icon(icon_style)    
+    bread_content = content_tag(:h2, icon + "  " + title + "  " + subtitle_content, :class => "page-title txt-color-blueDark")
+    bread = content_tag(:div, bread_content, :id => "breadcrumbs", :class => "col-xs-12 col-sm-7 col-md-7 col-lg-12")
+    content_tag(:div, bread, :class => "row")
+  end
 end

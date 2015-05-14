98d498cf41b09d0ed9c10055ea63f0ae971a5073
Thomas Wong <thomas.wong@laxino.com>
2015-05-14 12:26:29 +0800

    Improve roll shift effect

--- app/assets/javascripts/custom.effect.js
+++ app/assets/javascripts/custom.effect.js
@@ -4,11 +4,46 @@ function slideDownUpdate(element, newdata) {
   reshowWidget = function(widget) {
     setTimeout(function() {
       widget.html(newdata);
-      //widget.removeAttr( "style" ).hide().fadeIn();
       widget.removeAttr( "style" ).hide();
       widget.show("slide", { direction: "up" }, 500);
     }, 1000);
   }
-  //widget.animate({ opacity: 0 }, 900, reshowWidget(widget));
-  widget.hide("slide", { direction: "down" }, 500, reshowWidget(widget));
+
+  widget.hide("slide", { direction: "down" }, 500, reshowWidget(widget))
+}
+
+function animateUpdate(element, newdata) {
+  var widget = $(element);
+  var animatingClass = 'animating';
+  widget.addClass(animatingClass);
+
+  function setIntervalX(callback, delay, repetitions, afterCallback) {
+    var repeated = 0;
+    var intervalID = window.setInterval(function() {
+      console.log(repeated);
+      callback();
+
+      if ( ++repeated === repetitions ) {
+        window.clearInterval(intervalID);
+
+        window.setTimeout(afterCallback, 500);
+      }
+    }, delay);
+  }
+
+  var flashInterval = 1000;
+  var flashTime = 3;
+
+  setIntervalX(function() {
+                 $(element).effect("highlight", {color: '#A6A6A6'}, flashInterval);
+               },
+               flashInterval,
+               flashTime,
+               function() {
+                 slideDownUpdate(element, newdata);
+               });
+
+  setTimeout(function() {
+    widget.removeClass(animatingClass);
+  }, flashTime * flashInterval + 2000);
 }

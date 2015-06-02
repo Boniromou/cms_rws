dffa3ecebefaab90a6274d407bde8b525904e3fb
Thomas Wong <thomas.wong@laxino.com>
2015-06-02 18:02:49 +0800

    Call iwms when Deposit and Withdraw

--- app/controllers/fund_in_controller.rb
+++ app/controllers/fund_in_controller.rb
@@ -10,4 +10,8 @@ class FundInController < FundController
   def action_str
     "deposit"
   end
+
+  def call_iwms(member_id, amount, ref_trans_id, trans_date)
+    iwms_requester.deposit(member_id, amount, ref_trans_id, trans_date)
+  end
 end
--- app/controllers/fund_out_controller.rb
+++ app/controllers/fund_out_controller.rb
@@ -22,4 +22,8 @@ class FundOutController < FundController
   def handle_balance_not_enough(e)
     handle_fund_error({ key: "invalid_amt.no_enough_to_withdrawal", replace: { balance: @player.balance_str} })
   end
+
+  def call_iwms(member_id, amount, ref_trans_id, trans_date)
+    iwms_requester.withdraw(member_id, amount, ref_trans_id, trans_date)
+  end
 end

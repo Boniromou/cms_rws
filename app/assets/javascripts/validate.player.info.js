64f000290d8c077c155886c3c27fc7130e621707
Ming Wong <ming.wong@laxino.com>
2015-06-17 17:03:43 +0800

    refactor edit player and create player form

---
+++ app/assets/javascripts/validate.player.info.js
@@ -0,0 +1,55 @@
+  $(document).ready(function() {
+    $('input[autofocus="autofocus"]').focus();
+  });
+
+  function formValidated() {
+    function validated(comp, index) {
+
+      if ( comp.val() == "" )
+        return false;
+
+      if ( lengthLimit[index] > 0 && comp.val().length != lengthLimit[index] )
+        return false;
+
+      return true;
+    }
+
+    function targetLabel(index) {
+      return $("form#"+ form_id +" > fieldset > div.row:eq(" + index + ") > div.col:eq(2) label");
+    }
+
+    function showError(comp, index) {
+      targetLabel(index).css('visibility', 'visible');
+      comp.parent().addClass('state-error');
+    }
+
+    function hideError(comp, index) {
+      targetLabel(index).css('visibility', 'hidden');
+      comp.parent().removeClass('state-error');
+    }
+
+    var allValidated = true;
+
+    for (var i = 0; i < formComps.length; i++) {
+      var comp = $(formComps[i]);
+
+      if ( !validated(comp, i) ) {
+        allValidated = false;
+
+        showError(comp, i);
+      }
+      else {
+        hideError(comp, i);
+      }
+    }
+
+    return allValidated;
+  }
+
+  submit_button.click(function(e) {
+    e.preventDefault();
+
+    if ( formValidated() ) {
+      $("form#"+ form_id).submit();
+    }
+  });

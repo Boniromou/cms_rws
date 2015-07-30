0dbcd1aa1e28c2b68769e2df4bf908f7735e41b6
Ming Wong <ming.wong@laxino.com>
2015-07-30 11:10:05 +0800

    add dark blackground when pop up

--- app/assets/javascripts/popup.panel.js
+++ app/assets/javascripts/popup.panel.js
@@ -1,12 +1,17 @@
   $('#pop_up_dialog #cancel').click(function() {
+    $('#pop_up_dialog').removeClass("fadeIn");
+    $('#pop_up_dialog').addClass("fadeOut");
+    setTimeout(function(){
     $('#pop_up_dialog').css("display", "none");
-
+    },600);
     return false;
   });
 
   function registerPopUpPanel(form_id, content){
     $('#pop_up_content').html(content);
     $('#pop_up_dialog').css("display", "block");
+    $('#pop_up_dialog').removeClass("fadeOut");
+    $('#pop_up_dialog').addClass("fadeIn");
     $('#pop_up_dialog #confirm').focus();
   
     $('#pop_up_dialog #confirm').click(function() {

7764dd4fbf1d13e5bd80311bffd3bad33f86c9b2
Ming Wong <ming.wong@laxino.com>
2015-08-07 14:44:20 +0800

    sd shift_id, station_id, employee_id to iwms when deposit/withdraw

--- app/controllers/fund_out_controller.rb
+++ app/controllers/fund_out_controller.rb
@@ -17,7 +17,7 @@ class FundOutController < FundController
     handle_fund_error({ key: "invalid_amt.no_enough_to_withdrawal", replace: { balance: to_formatted_display_amount_str(e.message)} })
   end
 
-  def call_iwms(member_id, amount, ref_trans_id, trans_date)
-    iwms_requester.withdraw(member_id, amount, ref_trans_id, trans_date)
+  def call_iwms(member_id, amount, ref_trans_id, trans_date, shift_id, station_id, employee_id)
+    iwms_requester.withdraw(member_id, amount, ref_trans_id, trans_date, shift_id, station_id, employee_id)
   end
 end

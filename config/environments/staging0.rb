aff5f8b27d518a11fac4d30396450d3186e7fbbf
Marcus Ao <marcus.ao@laxino.com>
2015-09-14 15:15:21 +0800

    fix wallet url and port

--- config/environments/staging0.rb
+++ config/environments/staging0.rb
@@ -47,5 +47,5 @@ end
 
 SSO_URL = 'http://mo-stg-sso-vapp01.rnd.laxino.com:80'
 REGISTRATION_PATH = '/register'
-WALLET_URL = 'http://mo-stg-cwms-vapp01.rnd.laxino.com:10102'
+WALLET_URL = 'http://mo-stg-cwms-vapp01.rnd.laxino.com:80'
 WALLET_PATH = '/cwms'

4cfea6fd40b767d2c193db3b2cae20cd04360fe2
Ming Wong <ming.wong@laxino.com>
2015-10-05 15:07:41 +0800

    fix PlayersLockTypes find or create criteria

--- app/models/players_lock_type.rb
+++ app/models/players_lock_type.rb
@@ -8,18 +8,18 @@ class PlayersLockType < ActiveRecord::Base
   
   class << self
     def add_lock_to_player(player_id, lock_type_name)
-      lock_type = LockType.find_by_name(lock_type_name)
-      raise 'lock type not found' unless lock_type
-      @players_lock_type = PlayersLockType.where(:player_id => player_id, :lock_type_id => lock_type.id).first_or_create
-      @players_lock_type.status = STATUS_ACTIVE
-      @players_lock_type.save
+      create_or_find_lock(player_id, lock_type_name, STATUS_ACTIVE)
     end
     
     def remove_lock_to_player(player_id, lock_type_name)
+      create_or_find_lock(player_id, lock_type_name, STATUS_INACTIVE)
+    end
+
+    def create_or_find_lock(player_id, lock_type_name, status)
       lock_type = LockType.find_by_name(lock_type_name)
       raise 'lock type not found' unless lock_type
-      @players_lock_type = PlayersLockType.where(:player_id => player_id, :lock_type_id => lock_type.id).first_or_create
-      @players_lock_type.status = STATUS_INACTIVE
+      @players_lock_type = PlayersLockType.where(:player_id => player_id, :lock_type_id => lock_type.id, :status => [STATUS_ACTIVE,STATUS_INACTIVE]).first_or_create
+      @players_lock_type.status = status
       @players_lock_type.save
     end
   end

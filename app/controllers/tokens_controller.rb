bef061114a921ba7d1a0e00b4be63af6827594ee
Ming Wong <ming.wong@laxino.com>
2015-10-15 10:56:37 +0800

    verify the request header

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,7 +2,7 @@ class TokensController < ApplicationController
   skip_before_filter :check_session_expiration, :authenticate_user!, :pass_terminal_id
   include Hood::RWSHandler
 
-  config_handler RequestHandler.instance,false
+  config_handler RequestHandler.instance,true
   config_handler_backtrace_cleaner Rails.backtrace_cleaner
 
   def validate

b6f4282ed3cf53fe9333bc2aa7c6daaf6f464b14
Ming Wong <ming.wong@laxino.com>
2015-10-15 16:12:07 +0800

    set e.message to float when handle balance not enough

--- app/controllers/fund_out_controller.rb
+++ app/controllers/fund_out_controller.rb
@@ -14,7 +14,7 @@ class FundOutController < FundController
   end
 
   def handle_balance_not_enough(e)
-    handle_fund_error({ key: "invalid_amt.no_enough_to_withdrawal", replace: { balance: to_formatted_display_amount_str(e.message)} })
+    handle_fund_error({ key: "invalid_amt.no_enough_to_withdrawal", replace: { balance: to_formatted_display_amount_str(e.message.to_f)} })
   end
 
   def call_wallet(member_id, amount, ref_trans_id, trans_date, shift_id, station_id, user_id)

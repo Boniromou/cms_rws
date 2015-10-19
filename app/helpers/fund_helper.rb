a17ac6e8d3d7ab65737c06537c95c1c6d83ef6a3
Ming Wong <ming.wong@laxino.com>
2015-10-19 15:36:34 +0800

    update fund-related errors

--- app/helpers/fund_helper.rb
+++ app/helpers/fund_helper.rb
@@ -1,6 +1,6 @@
 module FundHelper
   def validate_amount_str( amount )
-    raise AmountInvalidError.new "Input amount not valid" unless amount.is_a?(String) && amount =~ /^\d{1,7}(\.\d{1,2})?$/ && to_server_amount( amount ) > 0
+    raise FundInOut::AmountInvalidError.new "Input amount not valid" unless amount.is_a?(String) && amount =~ /^\d{1,7}(\.\d{1,2})?$/ && to_server_amount( amount ) > 0
   end
 
   def to_server_amount( amount )
@@ -24,8 +24,5 @@ module FundHelper
     str = ("0x%08x" % (id + 0x80000000))
     str = str[2, str.length - 2] if str.start_with?('0x')
     "C#{str.upcase}"
-  end
-
-  class AmountInvalidError < Exception
   end
 end

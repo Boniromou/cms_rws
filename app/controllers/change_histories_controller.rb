fd883a1f05fe2f4cac7a2518b8aa6cc74b8a514d
Marcus Ao <marcus.ao@laxino.com>
2015-10-22 10:14:32 +0800

    add change history controller, model and views

---
+++ app/controllers/change_histories_controller.rb
@@ -0,0 +1,35 @@
+class ChangeHistoriesController < ApplicationController
+  layout 'cage'
+  include FormattedTimeHelper
+  include FrontMoneyHelper
+
+  def search
+    return unless permission_granted? Shift.new, :search_fm?
+
+    @accounting_date = params[:accounting_date] || current_accounting_date.accounting_date
+
+  end
+
+  def do_search
+    return unless permission_granted? Shift.new, :search_fm?
+    begin
+    accounting_date = params[:accounting_date]
+    @accounting_date = parse_date(accounting_date, current_accounting_date.accounting_date)
+    accounting_date_id = AccountingDate.get_id_by_date(@accounting_date)
+    start_time = Shift.where(:accounting_date_id => accounting_date_id).order(:created_at).first.created_at
+    end_time = Shift.where(:accounting_date_id => accounting_date_id).order(:created_at).last.roll_shift_at
+    start_time = Time.now.utc unless start_time
+    end_time = Time.now.utc unless end_time
+    # raise FrontMoneyHelper::NoResultException.new "shift not found" if start_shift.nil? || end_shift.nil?
+
+    @change_histories = ChangeHistory.by_property_id(current_user.property_id).since(start_time).until(end_time)
+    p @change_histories
+    rescue FrontMoneyHelper::NoResultException => e
+      @change_histories = []
+    end
+    respond_to do |format|
+      format.html { render partial: "change_histories/search_result", formats: [:html] }
+      format.js { render partial: "change_histories/search_result", formats: [:js] }
+    end
+  end
+end

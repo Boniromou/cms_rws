7c2043f5be15f1e99d7a90099356ee9e90bb374c
Marcus Ao <marcus.ao@laxino.com>
2015-10-26 09:33:16 +0800

    add cronjob for auto roll shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -27,6 +27,24 @@ class Shift < ActiveRecord::Base
     end
   end
 
+  def roll_by_system
+    raise 'rolled_error' if self.roll_shift_at != nil
+
+    self.roll_shift_on_station_id = 1
+    self.roll_shift_by_user_id = 1
+    self.roll_shift_at = Time.now.utc
+
+    new_shift = Shift.new
+    new_shift_name = self.class.next_shift_name_by_name(name)
+    new_shift.shift_type_id = ShiftType.get_id_by_name(new_shift_name)
+    new_shift.accounting_date_id = AccountingDate.next_shift_accounting_date_id(name)
+
+    Shift.transaction do
+      self.save
+      new_shift.save
+    end
+  end
+
   class << self
     def instance
       @shift = Shift.new unless @shift
---
+++ cronjob/roll_shift.rb
@@ -0,0 +1,28 @@
+require 'active_record'
+
+require File.expand_path("../../config/initializers/application", __FILE__)
+require File.expand_path("../../app/helpers/fund_helper", __FILE__)
+require File.expand_path("../../app/helpers/front_money_helper", __FILE__)
+require File.expand_path("../../app/models/shift", __FILE__)
+require File.expand_path("../../app/models/shift_type", __FILE__)
+require File.expand_path("../../app/models/properties_shift_type", __FILE__)
+require File.expand_path("../../app/models/accounting_date", __FILE__)
+
+
+
+env = $*[0] || "development"
+database = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml'))
+DB = database[env]
+ActiveRecord::Base.establish_connection(:adapter => "mysql2",
+                                        :host => DB['host'],
+                                        :username => DB['username'],
+                                        :password => DB['password'],
+                                        :database => DB['database'],
+                                        :port => DB['port'])
+puts "*************** #{Time.now.utc} ****************"
+puts "Start rolling shift"
+current_shift = Shift.current
+current_shift.roll_by_system
+puts "Finish rolling shift"
+puts "*************** #{Time.now.utc} ****************"
+

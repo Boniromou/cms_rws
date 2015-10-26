a7a7a890dd2325363cb9085fc372a983cef73522
Ming Wong <ming.wong@laxino.com>
2015-10-26 14:46:39 +0800

    update gen test case report

--- at/script/gen_test_report.sh
+++ at/script/gen_test_report.sh
@@ -4,12 +4,14 @@ rspec -fh spec/features/front_money_controller_spec.rb -o at/report/front_money_
 rspec -fh spec/features/fund_controller_spec.rb -o at/report/fund_controller_spec.html
 rspec -fh spec/features/fund_in_controller_spec.rb -o at/report/fund_in_controller_spec.html
 rspec -fh spec/features/fund_out_controller_spec.rb -o at/report/fund_out_controller_spec.html
+rspec -fh spec/features/void_controller_spec.rb -o at/report/void_controller_spec.html
 rspec -fh spec/features/locations_controller_spec.rb -o at/report/locations_controller_spec.html
 rspec -fh spec/features/players_controller_spec.rb -o at/report/players_controller_spec.html
 rspec -fh spec/features/player_transactions_controller_spec.rb -o at/report/player_transactions_controller_spec.html
 rspec -fh spec/features/shifts_controller_spec.rb -o at/report/shifts_controller_spec.html
 rspec -fh spec/features/stations_controller_spec.rb -o at/report/stations_controller_spec.html
 rspec -fh spec/features/user_session_spec.rb -o at/report/user_session_spec.html
+rspec -fh spec/features/terminals_controller_spec.rb -o at/report/terminals_controller_spec.html
 
 
 

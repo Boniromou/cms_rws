c8a34de178fabdb93b5cc740fac47fa3872828dd
Marcus Ao <marcus.ao@laxino.com>
2015-10-27 16:22:29 +0800

    fix auto roll shift cronjob

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -7,4 +7,4 @@ AUDIT_LOG_SEARCH_RANGE = Configuration.retrieve_config('audit_log_search_range')
 CHANGE_LOG_SEARCH_RANGE = Configuration.retrieve_config('change_log_search_range')
 TOKEN_LIFE_TIME = Configuration.retrieve_config('token_life_time') * 60
 SESSION_EXPIRATION_TIME = Configuration.retrieve_config('session_life_time') * 60
-ROLL_SHIFT_TIME = Configuration.retrieve_config('roll_shift_time')
+ROLL_SHIFT_TIME = Configuration.find_by_key_and_property_id('roll_shift_time', PROPERTY_ID).value

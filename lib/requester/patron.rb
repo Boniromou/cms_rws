b1f53e02903743bf04f23977811dd1524c41a6ad
Ming Wong <ming.wong@laxino.com>
2015-11-02 16:44:47 +0800

    add API get_player_infos to patron requester

--- lib/requester/patron.rb
+++ lib/requester/patron.rb
@@ -8,6 +8,11 @@ class Requester::Patron < Requester::Standard
       self.send "parse_#{__callee__}_response", response
     end
   end
+  
+  def get_player_infos(member_ids)
+    response = remote_rws_call('get', "#{@path}/#{__callee__}", :body => {:member_ids => member_ids})
+    self.send "parse_#{__callee__}_response", response
+  end
 
   protected
 
@@ -17,5 +22,14 @@ class Requester::Patron < Requester::Standard
     error_code = result_hash[:error_code].to_s
     raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" unless ['OK'].include?(error_code)
     return result_hash
+  end
+
+  def parse_get_player_infos_response(result)
+    result_hash = remote_response_checking(result, :error_code)
+    error_code = result_hash[:error_code].to_s
+    raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" unless ['OK'].include?(error_code)
+    player_info_array = result_hash[:player_infos]
+    raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" if player_info_array.nil?
+    return player_info_array
   end
 end

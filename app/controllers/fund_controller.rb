72291a73f84bc8257cab542a3d5368d0173288d0
Marcus Ao <marcus.ao@laxino.com>
2015-11-03 12:41:19 +0800

    validate pin when withdraw

--- app/controllers/fund_controller.rb
+++ app/controllers/fund_controller.rb
@@ -38,6 +38,8 @@ class FundController < ApplicationController
     end
 
     amount = params[:player_transaction][:amount]
+    pin = params[:player_transaction][:pin]
+    PlayerInfo.validate_pin(@member_id, pin) if action_str == 'fund_out'
     server_amount = get_server_amount(amount)
     AuditLog.fund_in_out_log(action_str, current_user.name, client_ip, sid,:description => {:station => current_station, :shift => current_shift.name}) do
       @transaction = do_fund_action(@member_id, server_amount)

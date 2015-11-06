01fc7abfeb25fd46c10892eb363b2b21e40027b6
Ming Wong <ming.wong@laxino.com>
2015-11-06 11:47:59 +0800

    slip number start from 10001

--- db/migrate/20151026090209_insert_transaction_slips_init_data.rb
+++ db/migrate/20151026090209_insert_transaction_slips_init_data.rb
@@ -1,7 +1,7 @@
 class InsertTransactionSlipsInitData < ActiveRecord::Migration
   def up
-    execute "INSERT INTO transaction_slips(property_id, slip_type_id, next_number, created_at, updated_at) values(20000, 1, 1, '#{Time.now.utc}', '#{Time.now.utc}');"
-    execute "INSERT INTO transaction_slips(property_id, slip_type_id, next_number, created_at, updated_at) values(20000, 2, 1, '#{Time.now.utc}', '#{Time.now.utc}');"
+    execute "INSERT INTO transaction_slips(property_id, slip_type_id, next_number, created_at, updated_at) values(20000, 1, 10001, '#{Time.now.utc}', '#{Time.now.utc}');"
+    execute "INSERT INTO transaction_slips(property_id, slip_type_id, next_number, created_at, updated_at) values(20000, 2, 10001, '#{Time.now.utc}', '#{Time.now.utc}');"
   end
 
   def down

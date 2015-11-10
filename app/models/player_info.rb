d31e49e4517351e54351e065ad40baf15d3d3bd2
Marcus Ao <marcus.ao@laxino.com>
2015-11-10 10:30:54 +0800

    apply validate_pin and validate_machine_token requesters

--- app/models/player_info.rb
+++ app/models/player_info.rb
@@ -6,30 +6,35 @@ class PlayerInfo
     end
 
     def retrieve_info(card_id, machine_token, pin, property_id)
-      @wallet_requester = Requester::Standard.new(PROPERTY_ID, 'test_key', WALLET_URL + WALLET_PATH) unless @wallet_requester
-      return {:status => 400, :error_code => 'InvalidMachineToken', :error_msg => 'Machine token is invalid'} unless validate_machine_token(machine_token, property_id)
-      player = Player.find_by_card_id_and_property_id(card_id, property_id)
-      return {:status => 400, :error_code => 'InvalidCardId', :error_msg => 'Card id is not exist'} unless player
-      return {:status => 400, :error_code => 'PlayerLocked', :error_msg => 'Player is locked'} if player.account_locked?
-      login_name = player.member_id
-      return {:status => 400, :error_code => 'InvalidPin', :error_msg => 'Pin is wron with card id'} unless validate_pin(login_name, pin)
-      currency = player.currency.name
-      balance = @wallet_requester.get_player_balance(player.member_id)
-      return {:status => 500, :error_code => 'RetrieveBalanceFail', :error_msg => 'Retrieve balance from wallet fail'} unless balance.class == Float
-      session_token = Token.generate(player.id).session_token
+      begin
+        @wallet_requester = Requester::Standard.new(PROPERTY_ID, 'test_key', WALLET_URL + WALLET_PATH) unless @wallet_requester
+        raise Request::InvalidMachineToken.new  unless validate_machine_token(machine_token, property_id)
+        player = Player.find_by_card_id_and_property_id(card_id, property_id)
+        raise Request::InvalidCardId.new unless player
+        raise Request::PlayerLocked.new if player.account_locked?
+        login_name = player.member_id
+        validate_pin(login_name, pin)
+        currency = player.currency.name
+        balance = @wallet_requester.get_player_balance(player.member_id)
+        raise Request::RetrieveBalanceFail.new unless balance.class == Float
+        session_token = Token.generate(player.id).session_token
 
-      {:login_name => login_name, :currency => currency, :balance => balance, :session_token => session_token}
+        {:login_name => login_name, :currency => currency, :balance => balance, :session_token => session_token}
+      rescue Remote::PinError
+        raise Request::InvalidPin.new
+      rescue Remote::PlayerNotFound
+        raise Request::InvalidPin.new
+      end
     end
 
     def validate_machine_token(machine_token, property_id)
-      # response = Machine.validate(machine_token, property_id)
-      # return false if response[:error_code] != 'OK' 
-      true
+      response = Machine.validate(machine_token, property_id)
+      return true if response[:error_code] == 'OK'
+      false
     end
 
     def validate_pin(login_name, pin)
-      #TODO validate pin
-      true
+      patron_requester.validate_pin(login_name, pin)
     end
 
     def get_currency(login_name, property_id)

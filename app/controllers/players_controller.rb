a2a2477feb3ef5613c6817e74292e3ec9d6f3175
Marcus Ao <marcus.ao@laxino.com>
2015-11-13 09:42:45 +0800

    fix cancel button cannot return

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   layout 'cage'
   rescue_from PlayerProfile::PlayerNotFound, :with => :handle_player_not_found
   rescue_from Remote::PlayerNotFound, :with => :handle_player_not_found
-  rescue_from PlayerProfile::PlayerNotActivated, :with => :player_not_activated
+  rescue_from PlayerProfile::PlayerNotActivated, :with => :handle_player_not_activated
 
   def new
     return unless permission_granted? :Player
@@ -42,10 +42,11 @@ class PlayersController < ApplicationController
     player_info
   end
 
-  def player_not_activated(e)
+  def handle_player_not_activated(e)
     @player_balance = 'no_balance'
     @inactivate = true
     @player = e.player
+    @operation = params[:operation]
 
     respond_to do |format|
       format.html { render "players/player_info", formats: [:html] }
@@ -143,27 +144,65 @@ class PlayersController < ApplicationController
   end
 
   def create_pin
-    @operation = 'create_pin'
+    @action = 'create_pin'
+    @inactivate = true
+    @player = Player.new(:member_id => params[:member_id], :card_id => params[:card_id], :status => 'not_activated')
+    @operation = params[:operation]
     set_pin
   end
 
   def reset_pin
-    @operation = 'reset_pin'
+    @action = 'reset_pin'
+    @inactivate = false
+    member_id = params[:member_id]
+    @player = Player.find_by_member_id(member_id)
     set_pin
   end
 
   def set_pin
-    member_id = params[:member_id]
-    @player = Player.find_by_member_id(member_id)
+    return unless permission_granted? :Player, :reset_pin?
+    
     respond_to do |format|
       format.html { render "players/set_pin", formats: [:html] }
-      format.js { render"players/set_pin", formats: [:js] }
+      format.js { render "players/set_pin", formats: [:js] }
     end
   end
 
   def do_reset_pin
-    return unless permission_granted? :Player, :unlock?
-    flash[:success] = { key: "reset_pin.set_pin_success", replace: {name: params[:player][:member_id]}}
-    redirect_to :action => 'profile', :member_id => params[:player][:member_id]
+    # TODO permission
+    return unless permission_granted? :Player, :reset_pin?
+    begin
+      audit_log = {:user => current_user.name, :member_id => params[:player][:member_id], :action_at => Time.now.utc, :action => params[:action].split('_')[0]}
+      response = patron_requester.reset_pin(params[:player][:member_id], params[:pin], audit_log)
+      if response.class != Hash
+        flash[:error] = "reset_pin.call_patron_fail"
+        Rails.logger.error "reset pin fail"
+        redirect_to_set_pin_path(params[:player][:member_id], params[:player][:card_id], params[:status], params[:inactivate], params[:operation])
+        return
+      end
+      Player.update_info(response[:player])
+      flash[:success] = { key: "reset_pin.set_pin_success", replace: {name: params[:player][:member_id]}}
+      redirect_to :action => 'profile', :member_id => params[:player][:member_id]
+    rescue Remote::PlayerNotFound
+      flash[:error] = "reset_pin.set_pin_fail"
+      redirect_to_set_pin_path(params[:player][:member_id], params[:player][:card_id], params[:status], params[:inactivate], params[:operation])
+    end
+  end
+
+  def player_not_activated
+    player = Player.new(:member_id => params[:member_id], :card_id => params[:card_id], :status => params[:status])
+    raise PlayerProfile::PlayerNotActivated.new(player)
+  end
+
+  protected
+  def redirect_to_set_pin_path(member_id, card_id, status, inactivate, operation)
+    @player = Player.find_by_member_id(member_id)
+    @player = Player.new(:member_id => member_id, :card_id => card_id, :status => status) unless @player 
+    @inactivate = inactivate
+    @operation = operation
+    respond_to do |format|
+      format.html { render "players/set_pin", formats: [:html] }
+      format.js { render"players/set_pin", formats: [:js] }
+    end
   end
 end

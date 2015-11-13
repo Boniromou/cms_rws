bdcaf56f65e9045b38594862f9528d916c986b26
Marcus Ao <marcus.ao@laxino.com>
2015-11-13 13:52:25 +0800

    fix error message bug

--- lib/requester/patron.rb
+++ lib/requester/patron.rb
@@ -41,6 +41,7 @@ class Requester::Patron < Requester::Standard
     #return {:member_id => 123, :card_id => 102130320923, :blacklist => true, :pin_status => 'null'}
     result_hash = remote_response_checking(result, :error_code)
     error_code = result_hash[:error_code].to_s
+    message = result_hash[:error_msg].to_s || "no message"
     raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" unless ['OK'].include?(error_code)
     player_info = result_hash[:player]
     return player_info
@@ -49,6 +50,7 @@ class Requester::Patron < Requester::Standard
   def parse_get_player_infos_response(result)
     result_hash = remote_response_checking(result, :error_code)
     error_code = result_hash[:error_code].to_s
+    message = result_hash[:error_msg].to_s || "no message"
     raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" unless ['OK'].include?(error_code)
     player_info_array = result_hash[:players]
     raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" if player_info_array.nil?
@@ -58,17 +60,18 @@ class Requester::Patron < Requester::Standard
   def parse_validate_pin_response(result)
     result_hash = remote_response_checking(result, :error_code)
     error_code = result_hash[:error_code].to_s
-    error_msg = result_hash[:error_msg].to_s
-    raise Remote::PinError, "error_code #{error_code}: #{error_msg}" if ['InvalidPin'].include?(error_code)
+    message = result_hash[:error_msg].to_s || "no message"
+    raise Remote::PinError, "error_code #{error_code}: #{message}" if ['InvalidPin'].include?(error_code)
     player_info = result_hash[:player]
-    raise Remote::PlayerNotFound, "error_code #{error_code}: #{error_msg}" if player_info.nil?
-    raise Remote::PinError, "error_code #{error_code}: #{error_msg}" unless ['OK'].include?(error_code)
+    raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" if player_info.nil?
+    raise Remote::PinError, "error_code #{error_code}: #{message}" unless ['OK'].include?(error_code)
     return player_info
   end
 
   def parse_reset_pin_response(result)
     result_hash = remote_response_checking(result, :error_code)
     error_code = result_hash[:error_code].to_s
+    message = result_hash[:error_msg].to_s || "no message"
     raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" unless ['OK'].include?(error_code)
     player_info = result_hash[:player]
     raise Remote::PlayerNotFound, "error_code #{error_code}: #{message}" if player_info.nil?
@@ -78,6 +81,7 @@ class Requester::Patron < Requester::Standard
   def parse_get_pin_audit_logs_response(result)
     result_hash = remote_response_checking(result, :error_code)
     error_code = result_hash[:error_code].to_s
+    message = result_hash[:error_msg].to_s || "no message"
     raise Remote::InvalidTimeRange, "error_code #{error_code}: #{message}" if ['InvalidTimeRange'].include?(error_code)
     audit_log_array = result_hash[:audit_logs]
     raise Remote::NoPinAuditLog, "error_code #{error_code}: #{message}" if audit_log_array.nil?

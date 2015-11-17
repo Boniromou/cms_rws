f6badd020530def783df0e6548eabc18e4ce0761
Marcus Ao <marcus.ao@laxino.com>
2015-11-17 18:22:03 +0800

    set showing 50 records as default

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,5 +1,7 @@
 //=require plugin/datatables/jquery.dataTables.min
 //=require plugin/datatables/dataTables.bootstrap.min
 $(document).ready(function() {
-  $('#datatable_col_reorder').dataTable();
+  $('#datatable_col_reorder').dataTable({
+  	"pageLength": 50
+  	});
 })

0bb2a44156c5a57fd9bd62f7df0f5ad1cec5c448
Ming Wong <ming.wong@laxino.com>
2015-11-17 15:16:19 +0800

    fix void controller test case

--- spec/features/support/step_helper.rb
+++ spec/features/support/step_helper.rb
@@ -462,7 +462,7 @@ module StepHelper
 
   def create_void_transaction(transaction_id)
     target_transaction = PlayerTransaction.find(transaction_id)
-    transaction = PlayerTransaction.create!(:shift_id => target_transaction.shift_id, :player_id => target_transaction.player_id, :user_id => target_transaction.user_id, :transaction_type_id => target_transaction.transaction_type_id + 2, :status => "completed", :amount => target_transaction.amount, :station_id => target_transaction.station_id , :created_at => Time.now, :slip_number => target_transaction.slip_number + 1)
+    transaction = PlayerTransaction.create!(:shift_id => target_transaction.shift_id, :player_id => target_transaction.player_id, :user_id => target_transaction.user_id, :transaction_type_id => target_transaction.transaction_type_id + 2, :status => "completed", :amount => target_transaction.amount, :machine_token => target_transaction.machine_token , :created_at => Time.now, :slip_number => target_transaction.slip_number + 1)
   end
 
   def reset_slip_number
--- spec/features/void_controller_spec.rb
+++ spec/features/void_controller_spec.rb
@@ -28,18 +28,16 @@ describe VoidController do
     end
     
     def create_player_transaction
-      @location6 = Location.create!(:name => "LOCATION6", :status => "active")
-      @station6 = Station.create!(:name => "STATION6", :status => "active", :location_id => @location6.id)
-      @player_transaction1 = PlayerTransaction.create!(:shift_id => Shift.last.id, :player_id => @player.id, :user_id => User.first.id, :transaction_type_id => 1, :status => "completed", :amount => 10000, :station_id => @station6.id, :created_at => Time.now, :slip_number => 1)
+      @machine_token1 = '20000|1|LOCATION1|1|STATION1|1|machine1|6e80a295eeff4554bf025098cca6eb37'
+      @player_transaction1 = PlayerTransaction.create!(:shift_id => Shift.last.id, :player_id => @player.id, :user_id => User.first.id, :transaction_type_id => 1, :status => "completed", :amount => 10000, :machine_token => @machine_token1, :created_at => Time.now, :slip_number => 1)
     end
 
     it '[47.1] Display void button', :js => true do
       create_past_shift
       login_as_admin
-      @location6 = Location.create!(:name => "LOCATION6", :status => "active")
-      @station6 = Station.create!(:name => "STATION6", :status => "active", :location_id => @location6.id)
-      @player_transaction2 = PlayerTransaction.create!(:shift_id => @past_shift.id, :player_id => @player.id, :user_id => User.first.id, :transaction_type_id => 1, :status => "completed", :amount => 10000, :station_id => @station6.id, :created_at => Time.now)
-      @player_transaction1 = PlayerTransaction.create!(:shift_id => Shift.last.id, :player_id => @player.id, :user_id => User.first.id, :transaction_type_id => 1, :status => "completed", :amount => 10000, :station_id => @station6.id, :created_at => Time.now)
+      @machine_token1 = '20000|1|LOCATION1|1|STATION1|1|machine1|6e80a295eeff4554bf025098cca6eb37'
+      @player_transaction2 = PlayerTransaction.create!(:shift_id => @past_shift.id, :player_id => @player.id, :user_id => User.first.id, :transaction_type_id => 1, :status => "completed", :amount => 10000, :machine_token => @machine_token1, :created_at => Time.now)
+      @player_transaction1 = PlayerTransaction.create!(:shift_id => Shift.last.id, :player_id => @player.id, :user_id => User.first.id, :transaction_type_id => 1, :status => "completed", :amount => 10000, :machine_token => @machine_token1, :created_at => Time.now)
       visit home_path
       click_link I18n.t("tree_panel.player_transaction") 
       check_player_transaction_page_js

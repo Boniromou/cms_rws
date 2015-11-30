7a6f8f4923b110dd4b3ba1f165f2d56b64ba4f40
Ming Wong <ming.wong@laxino.com>
2015-11-30 14:18:47 +0800

    get property keys from DB

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,11 @@
 class Property < ActiveRecord::Base
   attr_accessible :name, :secret_key
+
+  class << self
+    def get_property_keys
+      r = {}
+      Property.all.each {|p| r[p.id] = p.secret_key}
+      r
+    end
+  end
 end
--- config/initializers/request_handler.rb
+++ config/initializers/request_handler.rb
@@ -1,3 +1,3 @@
 service_config_file = "#{Rails.root}/config/service_config.yml"
 Hood::CONFIG.load_service_config(service_config_file,Rails.env)
-Hood::CONFIG.property_keys = {20000 => 'test_key'}
+Hood::CONFIG.property_keys = Property.get_property_keys

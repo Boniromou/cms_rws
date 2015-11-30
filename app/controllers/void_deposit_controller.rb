3e710c777102622cf7584419c5071d99773d64c4
Marcus Ao <marcus.ao@laxino.com>
2015-11-30 12:58:24 +0800

    delete useless params

--- app/controllers/void_deposit_controller.rb
+++ app/controllers/void_deposit_controller.rb
@@ -11,7 +11,7 @@ class VoidDepositController < VoidController
     "void_deposit"
   end
 
-  def call_wallet(member_id, amount, ref_trans_id, trans_date, shift_id, machine_token, user_id)
-    wallet_requester.void_deposit(member_id, amount, ref_trans_id, trans_date, shift_id, machine_token, user_id)
+  def call_wallet(member_id, amount, ref_trans_id, trans_date)
+    wallet_requester.void_deposit(member_id, amount, ref_trans_id, trans_date)
   end
 end

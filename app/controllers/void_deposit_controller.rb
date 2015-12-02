5b8106b460dcdd6d240dab3655404fe097a346d5
Ming Wong <ming.wong@laxino.com>
2015-12-02 10:21:12 +0800

    fix operation_str to void_deposit_controller

--- app/controllers/void_deposit_controller.rb
+++ app/controllers/void_deposit_controller.rb
@@ -3,6 +3,10 @@ class VoidDepositController < VoidController
     :void_deposit?
   end
 
+  def action_str
+    "void_deposit"
+  end
+
   def call_wallet(member_id, amount, ref_trans_id, trans_date)
     wallet_requester.void_deposit(member_id, amount, ref_trans_id, trans_date)
   end

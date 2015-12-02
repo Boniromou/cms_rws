880540b6c540ab25ee0d5696ef3dd4e9f9dcb7bd
Marcus Ao <marcus.ao@laxino.com>
2015-12-02 11:53:33 +0800

    fix problem about credit expired at param

--- app/controllers/credit_deposit_controller.rb
+++ app/controllers/credit_deposit_controller.rb
@@ -8,6 +8,7 @@ class CreditDepositController < FundController
   end
 
   def call_wallet(member_id, amount, ref_trans_id, trans_date)
-    wallet_requester.credit_deposit(member_id, amount, ref_trans_id, trans_date)
+  	credit_expired_at = Time.now.utc + CREDIT_LIFE_TIME
+    wallet_requester.credit_deposit(member_id, amount, ref_trans_id, trans_date, credit_expired_at)
   end
 end

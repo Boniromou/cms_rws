68d086b8aa211ff90208a534866415a2136d8a61
Marcus Ao <marcus.ao@laxino.com>
2015-12-03 18:16:01 +0800

    modify lock histories controller to support search accounting date rage

--- app/controllers/lock_histories_controller.rb
+++ app/controllers/lock_histories_controller.rb
@@ -2,29 +2,20 @@ class LockHistoriesController < ApplicationController
   layout 'cage'
   include FormattedTimeHelper
   include FrontMoneyHelper
+  include ApplicationHelper
 
   def search
     return unless permission_granted? :ChangeHistory, :lock_player_log?
 
-    @accounting_date = params[:accounting_date] || current_accounting_date.accounting_date
-    respond_to do |format|
-      format.html { render "lock_histories/search", formats: [:html] }
-      format.js { render "lock_histories/search", formats: [:js] }
-    end
+    @default_date = params[:accounting_date] || current_accounting_date.accounting_date
   end
 
   def do_search
     return unless permission_granted? :ChangeHistory, :lock_player_log?
     
     begin
-    accounting_date = params[:accounting_date]
-    @accounting_date = parse_date(accounting_date, current_accounting_date.accounting_date)
-    accounting_date_id = AccountingDate.get_by_date(@accounting_date).id
-    start_time = Shift.where(:accounting_date_id => accounting_date_id).order(:created_at).first.created_at
-    end_time = Shift.where(:accounting_date_id => accounting_date_id).order(:created_at).last.roll_shift_at
-    start_time = Time.now.utc unless start_time
-    end_time = Time.now.utc unless end_time
-
+    start_time, end_time = get_time_range_by_accounting_date(params[:start_time], params[:end_time], CHANGE_LOG_SEARCH_RANGE)
+    
     @lock_histories = ChangeHistory.by_property_id(current_user.property_id).since(start_time).until(end_time).where('action=? OR action=?', 'lock', 'unlock')
     rescue FrontMoneyHelper::NoResultException => e
       @lock_histories = []

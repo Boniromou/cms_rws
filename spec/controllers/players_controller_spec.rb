1aa3f0b65a691c693c5248e2769d9aa541896d3f
Ming Wong <ming.wong@laxino.com>
2015-12-08 16:15:28 +0800

    mock scope in controllers/players_controller_spec

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -17,7 +17,7 @@ describe PlayersController do
   describe 'Query Balance' do
     before(:each) do
       clean_dbs
-      @player = Player.create!(:first_name => "test", :last_name => "player", :member_id => 123456, :card_id => 1234567890, :currency_id => 1, :status => "active")
+      @player = Player.create!(:first_name => "test", :last_name => "player", :member_id => 123456, :card_id => 1234567890, :currency_id => 1, :status => "active", :property_id => 20000)
       controller.class.skip_before_filter :check_session_expiration, :authenticate_user!
       allow_any_instance_of(ApplicationController).to receive(:permission_granted?).and_return(true)
       bypass_rescue
@@ -29,7 +29,8 @@ describe PlayersController do
 
     it 'should be able to query balance' do
       allow_any_instance_of(Requester::Wallet).to receive(:get_player_balance).and_return({:balance => 99.99, :credit_balance => 0.0})
-
+      allow_any_instance_of(ApplicationPolicy::Scope).to receive(:resolve).and_return(Player.where(:property_id => 20000))
+      
       expect{ get 'balance', {member_id: 123456} }.to_not raise_error
     end
   end

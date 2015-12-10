2371d265b9b6d7d4dc722088e63ecbd7a55414c8
Marcus Ao <marcus.ao@laxino.com>
2015-12-10 11:07:32 +0800

    fix expiry time problem

--- app/controllers/credit_deposit_controller.rb
+++ app/controllers/credit_deposit_controller.rb
@@ -3,4 +3,9 @@ class CreditDepositController < FundController
   	credit_expired_at = Time.now.utc + config_helper.credit_life_time
     wallet_requester.credit_deposit(member_id, amount, ref_trans_id, trans_date, credit_expired_at)
   end
+
+   def new
+  	super
+    @credit_expired_at = Time.now.utc + config_helper.credit_life_time
+  end
 end

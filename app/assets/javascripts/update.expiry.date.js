291290fb0824400384fb939b450500b12e1fab2a
Ming Wong <ming.wong@laxino.com>
2016-02-02 14:56:46 +0800

    add update expiry date js

---
+++ app/assets/javascripts/update.expiry.date.js
@@ -0,0 +1,13 @@
+  function updateExpiryDate(element, dateTime){
+    element.html("Expiry: " + dateTime);
+  }
+
+  function onOptionChange(value){
+    element = $('label#credit_expired_at');
+    var now = new Date();
+    var expire_date = new Date(now.getTime() + 24*60*60*1000*value);
+    var date_str = getDateStr(expire_date) + ' ' + getTimeStrWithoutSecond(expire_date);
+    updateExpiryDate(element, date_str);
+  }
+  onOptionChange($("select#duration").val());
+  $('select#duration').attr("onchange","onOptionChange(value)");

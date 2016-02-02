25de1525324c8b2a62300bf6098e26635d09e2ae
Ming Wong <ming.wong@laxino.com>
2016-02-02 16:33:17 +0800

    add get permission attributes method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,19 @@ class User < ActiveRecord::Base
     user && user[:admin]
   end
 
+  def get_permission_attribute(target, action)
+    cache_key = "#{APP_NAME}:permissions:#{self.uid}"
+    permissions = Rails.cache.fetch cache_key
+    attributes = permissions[:permissions][:attributes]
+    if attributes.has_key(target)
+      attributes = attributes[target]
+      if attributes.has_key(action)
+        return attributes[action]
+      end
+    end
+    nil
+  end
+
   class << self
     def get_property_ids_by_uid(uid)
       user = Rails.cache.fetch "#{uid}"

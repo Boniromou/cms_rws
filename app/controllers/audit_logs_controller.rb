568248b52d1a346261d1fa9fcbc4f12b33fb6d0b
Ming Wong <ming.wong@laxino.com>
2016-02-11 16:41:13 +0800

    add audit log search range limit

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -10,13 +10,26 @@ class AuditLogsController < ApplicationController
   end
 
   def do_search
-    start_time = parse_search_time(params[:start_time]) unless params[:start_time].blank?
-    end_time = parse_search_time(params[:end_time], true) unless params[:end_time].blank?
-    action_by = params[:action_by] unless params[:action_by].blank?
-    action_type = params[:action_type] unless params[:action_type].blank?
-    audit_target = params[:target_name] unless params[:target_name].blank? || params[:target_name] == "all"
-    action = params[:action_list] unless params[:action_list].blank? || params[:action_list] == "all"
-    @audit_logs = AuditLog.search_query(audit_target, action, action_type, action_by, start_time, end_time)
+    begin
+      start_time = parse_search_time(params[:start_time]) unless params[:start_time].blank?
+      end_time = parse_search_time(params[:end_time], true) unless params[:end_time].blank?
+      date_gap = end_time - start_time
+      search_range = config_helper.audit_log_search_range
+      raise Search::OverRangeError, "limit_remark" if date_gap > search_range
+      action_by = params[:action_by] unless params[:action_by].blank?
+      action_type = params[:action_type] unless params[:action_type].blank?
+      audit_target = params[:target_name] unless params[:target_name].blank? || params[:target_name] == "all"
+      action = params[:action_list] unless params[:action_list].blank? || params[:action_list] == "all"
+      @audit_logs = AuditLog.search_query(audit_target, action, action_type, action_by, start_time, end_time)
+    rescue Search::NoResultException => e
+      @audit_logs = []
+    rescue Search::OverRangeError => e
+      flash[:error] = "report_search." + e.message
+    rescue Search::DateTimeError => e
+      flash[:error] = "transaction_history." + e.message
+    rescue ArgumentError 
+      flash[:error] = "transaction_history.datetime_format_not_valid"
+    end
     
     respond_to do |format|
       format.html { render partial: "audit_logs/search_result", formats: [:html] }

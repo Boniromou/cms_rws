578c8d588945030b4446817eddcf6d9d7352dd8c
Ming Wong <ming.wong@laxino.com>
2016-03-22 16:30:30 +0800

    add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Currency.where(:id => 2, :name => 'HKD').first_or_create
+
+Locktype.where(:id => 1, :name => 'cage_lock').first_or_create
+Locktype.where(:id => 2, :name => 'blacklist').first_or_create
+Locktype.where(:id => 3, :name => 'pending').first_or_create
+
+ShiftType.where(:id =>1, :name => 'morning').first_or_create
+ShiftType.where(:id =>2, :name => 'swing').first_or_create
+ShiftType.where(:id =>3, :name => 'night').first_or_create
+ShiftType.where(:id =>4, :name => 'day').first_or_create
+
+SlipType.where(:id => 1, :name => 'deposit').first_or_create
+SlipType.where(:id => 2, :name => 'withdraw').first_or_create
+
+TransactionType.where(:id => 1, :name => 'deposit').first_or_create
+TransactionType.where(:id => 2, :name => 'withdraw').first_or_create
+TransactionType.where(:id => 3, :name => 'void_deposit').first_or_create
+TransactionType.where(:id => 4, :name => 'void_withdraw').first_or_create
+TransactionType.where(:id => 5, :name => 'credit_deposit').first_or_create
+TransactionType.where(:id => 6, :name => 'credit_expire').first_or_create

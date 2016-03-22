b6db09c142aac59396dc0485413d17f4908a7e4a
Ming Wong <ming.wong@laxino.com>
2016-03-22 15:43:19 +0800

    fix migration script

--- db/migrate/20150323062455_create_currencies.rb
+++ db/migrate/20150323062455_create_currencies.rb
@@ -1,10 +1,14 @@
 class CreateCurrencies < ActiveRecord::Migration
-  def change
+  def up
     create_table :currencies do |t|
       t.string :name
 
       t.timestamps
     end
     execute "INSERT INTO currencies(id, name,created_at,updated_at) values(2, 'HKD','2015-03-24 00:00:00','2015-03-24 00:00:00');"
+  end
+
+  def down
+    drop_table :currencies
   end
 end
--- db/migrate/20160112043123_drop_location_station_table.rb
+++ db/migrate/20160112043123_drop_location_station_table.rb
@@ -7,13 +7,20 @@ class DropLocationStationTable < ActiveRecord::Migration
   def down
     create_table :stations do |t|
       t.string :name
+      t.string :terminal_id
+      t.integer :location_id
+      t.string :status
+      t.datetime :purge_at
 
       t.timestamps
     end
     create_table :locations do |t|
       t.string :name
+      t.string :status
+      t.datetime :purge_at
 
       t.timestamps
     end
+    execute "ALTER TABLE stations ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) REFERENCES locations(id);"
   end
 end

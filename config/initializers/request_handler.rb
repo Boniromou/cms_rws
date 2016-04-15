209e5f8d4f6a212a6b8489e852e5ad4565a02b2e
Ming Wong <ming.wong@laxino.com>
2016-04-15 12:25:50 +0800

    fix request_handler, get properties key when init

--- config/initializers/request_handler.rb
+++ config/initializers/request_handler.rb
@@ -1,3 +1,3 @@
 service_config_file = "#{Rails.root}/config/service_config.yml"
 Hood::CONFIG.load_service_config(service_config_file,Rails.env)
-#Hood::CONFIG.property_keys = Property.get_property_keys
+Hood::CONFIG.property_keys = Property.get_property_keys

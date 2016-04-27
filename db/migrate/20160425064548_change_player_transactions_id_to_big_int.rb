7003537df3a7a444e330e686543e0f403967cf7d
Ming Wong <ming.wong@laxino.com>
2016-04-27 10:51:05 +0800

    fix migration script, add auto_increment to player_transaction id

--- db/migrate/20160425064548_change_player_transactions_id_to_big_int.rb
+++ db/migrate/20160425064548_change_player_transactions_id_to_big_int.rb
@@ -1,9 +1,9 @@
 class ChangePlayerTransactionsIdToBigInt < ActiveRecord::Migration
   def up
-    change_column :player_transactions, :id, :bigint
+    change_column :player_transactions, :id, "BIGINT NOT NULL AUTO_INCREMENT"
   end
 
   def down
-    change_column :player_transactions, :id, :bigint
+    change_column :player_transactions, :id, "INTEGER NOT NULL AUTO_INCREMENT"
   end
 end

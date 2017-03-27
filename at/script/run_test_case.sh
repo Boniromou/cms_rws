361f6b271dd1d064f5c4120a71fb22e70492ada6
dennis.chio <dennis.chio@laxino.com>
2017-03-27 16:13:51 +0800

    change test case file name deposit/withdraw

--- at/script/run_test_case.sh
+++ at/script/run_test_case.sh
@@ -9,8 +9,8 @@ echo '******************************************************'
 rspec -cfd spec/features/audit_logs_controller_spec.rb 
 rspec -cfd spec/features/front_money_controller_spec.rb
 rspec -cfd spec/features/fund_controller_spec.rb
-rspec -cfd spec/features/fund_in_controller_spec.rb 
-rspec -cfd spec/features/fund_out_controller_spec.rb
+rspec -cfd spec/features/deposit_controller_spec.rb 
+rspec -cfd spec/features/withdraw_controller_spec.rb
 rspec -cfd spec/features/void_controller_spec.rb
 rspec -cfd spec/features/players_controller_spec.rb
 rspec -cfd spec/features/player_transactions_controller_spec.rb

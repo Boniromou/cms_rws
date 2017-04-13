9897f1949380a7a64426171bf1598fe33a9c2340
dennis.chio <dennis.chio@laxino.com>
2017-04-13 14:49:37 +0800

    fix over accounting date wrong

--- app/controllers/front_money_controller.rb
+++ app/controllers/front_money_controller.rb
@@ -21,13 +21,14 @@ class FrontMoneyController < ApplicationController
 
   def do_search
     begin
+      raise FrontMoneyHelper::NoResultException.new "shift not found" if (params[:select_casino] and is_date?(params[:accounting_date]) and (Time.parse(params[:accounting_date]) > current_accounting_date_by_casino_id(params[:select_casino]).accounting_date))
       accounting_date = parse_date(params[:accounting_date], current_accounting_date.accounting_date)
       @accounting_date = AccountingDate.get_by_date(accounting_date)
       start_shift = @accounting_date.shifts.first
       end_shift = @accounting_date.shifts.last
-      all_shift = params[:select_casino] ? Shift.get_shift_by_date_and_casino(@accounting_date.id, params[:select_casino])
+      all_shift = Shift.get_shift_by_date_and_casino(@accounting_date.id, params[:select_casino]) if params[:select_casino]
       raise FrontMoneyHelper::NoResultException.new "shift not found" if start_shift.nil? || end_shift.nil?
-      @player_transactions = params[:select_casino] ? PlayerTransaction.search_transactions_by_shift_id(current_user.id, all_shift) :  PlayerTransaction.search_transactions_by_user_and_shift(current_user.id, start_shift.id, end_shift.id)
+      @player_transactions = params[:select_casino] ? PlayerTransaction.search_transactions_by_shift_id(current_user.id, all_shift) : PlayerTransaction.search_transactions_by_user_and_shift(current_user.id, start_shift.id, end_shift.id)
       #@player_transactions = policy_scope(@player_transactions)
     rescue FrontMoneyHelper::NoResultException => e
       @player_transactions = []
@@ -36,5 +37,15 @@ class FrontMoneyController < ApplicationController
       format.html { render partial: "front_money/search_result", formats: [:html] }
       format.js { render partial: "front_money/search_result", formats: [:js] }
     end
+  end
+
+
+  def is_date?(date)
+    begin
+      Date.parse(date)
+    rescue ArgumentError
+      return false
+    end
+    true
   end
 end

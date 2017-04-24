fb139a26b78e4ac7f17c2cb5c45347255e97b24d
dennis.chio <dennis.chio@laxino.com>
2017-04-24 17:08:13 +0800

    Add search front money cannot select after accounting date

--- app/controllers/front_money_controller.rb
+++ app/controllers/front_money_controller.rb
@@ -21,15 +21,21 @@ class FrontMoneyController < ApplicationController
 
   def do_search
     begin
-      raise FrontMoneyHelper::NoResultException.new "shift not found" if (params[:select_casino] and is_date?(params[:accounting_date]) and (Time.parse(params[:accounting_date]) > current_accounting_date_by_casino_id(params[:select_casino]).accounting_date))
-      accounting_date = parse_date(params[:accounting_date], current_accounting_date.accounting_date)
-      @accounting_date = AccountingDate.get_by_date(accounting_date)
-      start_shift = @accounting_date.shifts.first
-      end_shift = @accounting_date.shifts.last
-      all_shift = Shift.get_shift_by_date_and_casino(@accounting_date.id, params[:select_casino]) if params[:select_casino]
-      raise FrontMoneyHelper::NoResultException.new "shift not found" if start_shift.nil? || end_shift.nil?
-      @player_transactions = params[:select_casino] ? PlayerTransaction.search_transactions_by_shift_id(current_user.id, all_shift) : PlayerTransaction.search_transactions_by_user_and_shift(current_user.id, start_shift.id, end_shift.id)
-      #@player_transactions = policy_scope(@player_transactions)
+      if (params[:select_casino] and is_date?(params[:accounting_date]) and (Time.parse(params[:accounting_date]) > current_accounting_date_by_casino_id(params[:select_casino]).accounting_date))
+        # Select date greater than current accounting date handle
+        accounting_date = parse_date(current_accounting_date_by_casino_id(params[:select_casino]).accounting_date.to_s, current_accounting_date.accounting_date)
+        @accounting_date = AccountingDate.get_by_date(accounting_date)
+        @player_transactions = []
+      else
+        accounting_date = parse_date(params[:accounting_date], current_accounting_date.accounting_date)
+        @accounting_date = AccountingDate.get_by_date(accounting_date)
+        start_shift = @accounting_date.shifts.first
+        end_shift = @accounting_date.shifts.last
+        all_shift = Shift.get_shift_by_date_and_casino(@accounting_date.id, params[:select_casino]) if params[:select_casino]
+        raise FrontMoneyHelper::NoResultException.new "shift not found" if start_shift.nil? || end_shift.nil?
+        @player_transactions = params[:select_casino] ? PlayerTransaction.search_transactions_by_shift_id(current_user.id, all_shift) : PlayerTransaction.search_transactions_by_user_and_shift(current_user.id, start_shift.id, end_shift.id)
+        #@player_transactions = policy_scope(@player_transactions)
+      end
     rescue FrontMoneyHelper::NoResultException => e
       @player_transactions = []
     end

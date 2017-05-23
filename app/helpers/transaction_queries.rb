3b95c9aef98edeff5818713b62dd2674dfdca6d6
dennis.chio <dennis.chio@laxino.com>
2017-05-23 11:27:11 +0800

    void yst trasaction did not include in daily limit

--- app/helpers/transaction_queries.rb
+++ app/helpers/transaction_queries.rb
@@ -49,7 +49,8 @@ module TransactionQueries
       start_shift_id = accounting_date.shifts.where(:casino_id => casino_id).first.id
       end_shift_id = accounting_date.shifts.where(:casino_id => casino_id).last.id
       trans_amt = select('sum(amount) as amount').by_player_id(player.id).by_casino_id(casino_id).by_status('completed').from_shift_id(start_shift_id).to_shift_id(end_shift_id).by_transaction_type_id(TRANSACTION_TYPE_ID_LIST[trans_type]).first.amount || 0
-      void_amt = select('sum(amount) as amount').by_player_id(player.id).by_casino_id(casino_id).by_status('completed').from_shift_id(start_shift_id).to_shift_id(end_shift_id).by_transaction_type_id(TRANSACTION_TYPE_ID_LIST["void_#{trans_type}".to_sym]).first.amount || 0
+      today_ref_trans_id = select('ref_trans_id').by_player_id(player.id).by_casino_id(casino_id).by_status('completed').from_shift_id(start_shift_id).to_shift_id(end_shift_id).by_transaction_type_id(TRANSACTION_TYPE_ID_LIST[trans_type])
+      void_amt = select('sum(amount) as amount').by_player_id(player.id).by_casino_id(casino_id).by_status('completed').from_shift_id(start_shift_id).to_shift_id(end_shift_id).by_transaction_type_id(TRANSACTION_TYPE_ID_LIST["void_#{trans_type}".to_sym]).where("ref_trans_id in (?)", today_ref_trans_id.map {|i| i.ref_trans_id }).first.amount || 0
       trans_amt - void_amt
     end
   end

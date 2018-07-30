05cdea17021b093e7300c587a38030319c1495b6
dennis.chio <dennis.chio@laxino.com>
2018-07-30 15:04:41 +0800

    typo

--- db/migrate/20180730091029_add_column_and_index_purge_at_to_change_histories.rb
+++ db/migrate/20180730091029_add_column_and_index_purge_at_to_change_histories.rb
@@ -1,11 +1,11 @@
 class AddColumnAndIndexPurgeAtToChangeHistories < ActiveRecord::Migration
   def up
-	add_column :audit_logs, :purge_at, :datetime
-    add_index :audit_logs, :purge_at, :name=>'idx_purge_at'
+	add_column :change_histories, :purge_at, :datetime
+    add_index :change_histories, :purge_at, :name=>'idx_purge_at'
   end
 
   def down
-    remove_index :audit_logs, :name=>'idx_purge_at'
-	remove_column :audit_logs, :purge_at, :datetime
+    remove_index :change_histories, :name=>'idx_purge_at'
+	remove_column :change_histories, :purge_at, :datetime
   end
 end

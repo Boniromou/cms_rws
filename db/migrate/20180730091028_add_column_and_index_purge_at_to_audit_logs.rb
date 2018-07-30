64f1112b82e29c339b9606dabac79f5d9a221e0b
dennis.chio <dennis.chio@laxino.com>
2018-07-30 15:00:41 +0800

    Add column and index in audit log and change histories

---
+++ db/migrate/20180730091028_add_column_and_index_purge_at_to_audit_logs.rb
@@ -0,0 +1,11 @@
+class AddColumnAndIndexPurgeAtToAuditLogs < ActiveRecord::Migration
+  def up
+	add_column :audit_logs, :purge_at, :datetime
+    add_index :audit_logs, :purge_at, :name=>'idx_purge_at'
+  end
+
+  def down
+    remove_index :audit_logs, :name=>'idx_purge_at'
+	remove_column :audit_logs, :purge_at, :datetime
+  end
+end

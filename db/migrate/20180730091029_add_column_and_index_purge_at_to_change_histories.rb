57e9092f0f658f0f366dbcb632c05cec2c2a0e0a
dennis.chio <dennis.chio@laxino.com>
2018-08-07 15:43:54 +0800

    Add two index

---
+++ db/migrate/20180730091029_add_column_and_index_purge_at_to_change_histories.rb
@@ -0,0 +1,12 @@
+class AddColumnAndIndexPurgeAtToChangeHistories < ActiveRecord::Migration
+  def up
+        add_column :change_histories, :purge_at, :datetime
+    add_index :change_histories, :purge_at, :name=>'idx_purge_at'
+  end
+
+  def down
+    remove_index :change_histories, :name=>'idx_purge_at'
+        remove_column :change_histories, :purge_at, :datetime
+  end
+end
+

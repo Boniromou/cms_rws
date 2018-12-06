9505347b558cbe8590004b09125e91aba8df45af
dennis.chio <dennis.chio@laxino.com>
2018-12-06 17:20:24 +0800

    add index

---
+++ db/migrate/20181205000001_add_index_updated_at_archive_tables.rb
@@ -0,0 +1,11 @@
+class AddIndexUpdatedAtArchiveLogs < ActiveRecord::Migration
+  def self.up
+    add_index :audit_logs, :updated_at, :name => 'idx_updated_at'
+	add_index :change_histories, :updated_at, :name => 'idx_updated_at'
+  end
+
+  def self.down
+    remove_index :change_histories, :name => 'idx_updated_at'
+    remove_index :archive_logs, :name => 'idx_updated_at'
+  end
+end

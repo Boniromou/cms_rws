74bd85340be46f69edc3049eac30e681f1a79e31
Xiaopan Yun <xiaopan.yun@laxino.com>
2018-12-09 20:38:31 +0800

    require export helper

--- config/initializers/require.rb
+++ config/initializers/require.rb
@@ -1,6 +1,7 @@
 require 'errors'
 require 'davis/davis_util'
 require 'rigi/pundit_helper'
+require 'excel/export_helper'
 Dir[Rails.root.join 'lib/core_ext/*.rb'].each {|file| require file }
 Dir[Rails.root.join 'lib/requester/*.rb'].each {|file| require file }
 Dir[Rails.root.join 'lib/excel/*.rb'].each {|file| require file }

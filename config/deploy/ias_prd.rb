02522e4bb01535f409f6219cec76f573c153f94c
dennis.chio <dennis.chio@laxino.com>
2018-12-21 15:17:28 +0800

    Add ias_prd config without repo

---
+++ config/deploy/ias_prd.rb
@@ -0,0 +1,26 @@
+# Define your release to be deployed to integration environment here.
+# Release number for integration environment is supposed to be odd numbers.
+set :branch, 'REL_1_2_3'
+
+# Define your repository server for integration environment here.
+#   production SVN - svn.prod.laxigames.com
+#   development SVN - svn.mo.laxino.com
+set :repo_host, 'git.lb.laxigames.local'
+
+# Define your application servers for integration environment here.
+#   int - Integration
+#   stg - Staging
+#   prd - Production
+role :app, 'do-prd-cms-app01.idc.local'
+
+role :cronjob_app, 'do-prd-cms-app01.idc.local'
+
+# Define your database servers for integration environment here.
+# role :db,  "int-cons-db01.rnd.laxino.com", :primary => true
+
+# Define your application cluster with Nginx settings here
+# These variables will be used in generating Nginx/Thin config files
+set :nginx_worker_processes, 2
+set :cluster_port, 10060
+set :virtual_server_name, 'do-prd-cms-vapp01.idc.local'
+set :num_of_servers, 12

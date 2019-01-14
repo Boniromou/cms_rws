d4e4fa0abd8c015822993fb4e0edfb620b416db8
sam.mok <sam.mok@gamesourcecloud.com>
2019-01-14 17:18:07 +0800

    exception db migration

---
+++ db/migrate/20190108022837_create_approval_logs.approval.rb
@@ -0,0 +1,19 @@
+#THis migration comes from approval (originally 20160927090242)
+class CreateApprovalLogs < ActiveRecord::Migration
+  def up
+    create_table :approval_logs do |t|
+      t.string  :action,    :limit => 45,  :null => false
+      t.string  :action_by, :limit => 255, :null => false
+      t.integer :approval_request_id, :null => false
+
+      t.timestamps
+    end
+
+    execute "ALTER TABLE approval_logs ADD CONSTRAINT fk_approvallogs_approvalrequests FOREIGN KEY (approval_request_id) REFERENCES approval_requests(id);"
+  end
+
+  def down
+    execute "ALTER TABLE approval_logs DROP FOREIGN KEY fk_approvallogs_approvalrequests;"
+    drop_table :approval_logs
+  end
+end
---
+++ db/migrate/20190108022838_change_data_limit_for_approval_requests.approval.rb
@@ -0,0 +1,10 @@
+#This migration comes from approval (originally 20170619025447)
+class ChangeDataLimitForApprovalRequests < ActiveRecord::Migration
+  def up
+    change_column :approval_requests, :data, :string, :limit => 4096
+  end
+
+  def down
+    change_column :approval_requests, :data, :string, :limit => 1024
+  end
+end

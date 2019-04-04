8daf0f439af039fbcf7161bc8511c49c89806a06
sam.mok <sam.mok@gamesourcecloud.com>
2019-04-04 17:54:16 +0800

    approval ui bug fix

--- app/policies/player_policy.rb
+++ app/policies/player_policy.rb
@@ -2,7 +2,7 @@ class PlayerPolicy < ApplicationPolicy
   policy_target :player
   map_policy :lock?, :unlock?, :reset_pin?
   map_policy :balance?, :action_name => :balance_enquiry
-  map_policy :merge?, :action_name => :merge_card
+  map_policy :merge?, :action_name => :merge
   map_policy :profile?, :action_name => :player_profile
   map_policy :create_pin?, :delegate_policies => [:reset_pin?]
   map_policy :do_reset_pin?, :delegate_policies => [:reset_pin?]

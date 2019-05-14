0f4d76bd298d30b3a65332f9e9532957ca68c976
xiaopan.yun <xiaopan.yun@gamesourcecloud.com>
2019-05-14 08:34:55 +0000

    upgrade kiosk deposit/withdraw api, add default payment_method and source_of_fund

--- app/models/kiosk_transaction.rb
+++ app/models/kiosk_transaction.rb
@@ -1,5 +1,5 @@
 class KioskTransaction < ActiveRecord::Base
-  attr_accessible :player_id, :shift_id, :transaction_type_id, :ref_trans_id, :amount, :status, :casino_id, :source_type, :kiosk_name, :trans_date, :created_at
+  attr_accessible :player_id, :shift_id, :transaction_type_id, :ref_trans_id, :amount, :status, :casino_id, :source_type, :kiosk_name, :trans_date, :created_at, :payment_method_id, :source_of_fund_id
   belongs_to :player
   belongs_to :shift
   belongs_to :transaction_type
@@ -35,14 +35,14 @@ class KioskTransaction < ActiveRecord::Base
   def cancelled?
     display_status == 'cancelled'
   end
-  
+
   def validated?
     display_status == 'validated'
   end
 
   class << self
   include FundHelper
-    def init_transaction(member_id, amount, trans_type, shift_id, kiosk_name, ref_trans_id, source_type, casino_id)
+    def init_transaction(member_id, amount, trans_type, shift_id, kiosk_name, ref_trans_id, source_type, casino_id, payment_method_type, source_of_fund)
       player = Player.find_by_member_id_and_licensee_id(member_id, Casino.find_by_id(casino_id).licensee_id)
       player_id = player[:id]
       transaction = new
@@ -56,16 +56,18 @@ class KioskTransaction < ActiveRecord::Base
       transaction[:casino_id] = casino_id
       transaction[:ref_trans_id] = ref_trans_id
       transaction[:trans_date] = Time.now
+      transaction[:payment_method_id] = payment_method_type
+      transaction[:source_of_fund_id] = source_of_fund
       transaction.save
       transaction
     end
 
-    def save_deposit_transaction(member_id, amount, shift_id, kiosk_name, ref_trans_id, source_type, casino_id)
-      init_transaction(member_id, amount, DEPOSIT, shift_id, kiosk_name, ref_trans_id, source_type, casino_id)
+    def save_deposit_transaction(member_id, amount, shift_id, kiosk_name, ref_trans_id, source_type, casino_id, payment_method_type = 2, source_of_fund = 7)
+      init_transaction(member_id, amount, DEPOSIT, shift_id, kiosk_name, ref_trans_id, source_type, casino_id, payment_method_type, source_of_fund)
     end
 
-    def save_withdraw_transaction(member_id, amount, shift_id, kiosk_name, ref_trans_id, source_type, casino_id)
-      init_transaction(member_id, amount, WITHDRAW, shift_id, kiosk_name, ref_trans_id, source_type, casino_id)
+    def save_withdraw_transaction(member_id, amount, shift_id, kiosk_name, ref_trans_id, source_type, casino_id, payment_method_type = 2, source_of_fund = 7)
+      init_transaction(member_id, amount, WITHDRAW, shift_id, kiosk_name, ref_trans_id, source_type, casino_id, payment_method_type, source_of_fund)
     end
   end
 end
